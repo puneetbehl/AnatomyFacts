096c9ac72b9cbd9e6a6fa5940a4375ef8b982682
Puneet <puneet.behl@intelligrape.com>
2012-10-04 15:09:29 +0530

    jquery added in application.js to validate that only one checkbox can be selected at a time

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -1,19 +1,25 @@
 if (typeof jQuery !== 'undefined') {
-	(function($) {
-		$('#spinner').ajaxStart(function() {
-			$(this).fadeIn();
-		}).ajaxStop(function() {
-			$(this).fadeOut();
-		});
-	})(jQuery);
+    (function ($) {
+        $('#spinner').ajaxStart(function () {
+            $(this).fadeIn();
+        }).ajaxStop(function () {
+                $(this).fadeOut();
+            });
+    })(jQuery);
 }
-$(document).ready(function() {
+$(document).ready(function () {
     $('.pagination').children().wrapAll('<ul>')
     $('.pagination ul').children().wrap('<li>')
     $('input[name=tags]').typeahead({
-        source: ['one', 'two', 'three'],
-        items: 3,
+        source:['one', 'two', 'three'],
+        items:3,
         minLength:2
 
     });
-})
+
+    $('.option').bind('click', function () {
+        $(this).parent().siblings().children('.option').attr('checked', false)
+    });
+
+
+});
