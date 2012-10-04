dec2bacc01dcc0b278e1dd2a3285ea5be68cd53c
Puneet <puneet.behl@intelligrape.com>
2012-10-04 12:19:13 +0530

    Quiz Question save action defined

--- grails-app/controllers/com/intelligrape/anatomyfacts/QuizQuestionController.groovy
+++ grails-app/controllers/com/intelligrape/anatomyfacts/QuizQuestionController.groovy
@@ -5,6 +5,15 @@ class QuizQuestionController {
     def scaffold = QuizQuestion
 
     def save() {
+        QuizQuestion question = new QuizQuestion(params)
+        if (question.save(failOnError: true))
+            flash.success = "<a href='${createLink(controller:'quizQuestion', action:'show', id:"${question.id}")}'>${question.id}</a> saved successfully"
+        redirect(action: 'create')
+    }
 
+    private List<Option> getOptionsFromParams(params) {
+        return params.option.findAll {!it.key.contains(".")}.collect {
+            return new Option(it.value as Map)
+        } as List
     }
 }
