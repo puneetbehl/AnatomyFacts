6b301d342d9c321cdc9f1e0ea88c8f20977405cc
Puneet <puneet.behl@intelligrape.com>
2012-10-05 11:20:02 +0530

    save action modified in QuizQuestion controller & defined in TestQuestion controller

--- grails-app/controllers/com/intelligrape/anatomyfacts/QuizQuestionController.groovy
+++ grails-app/controllers/com/intelligrape/anatomyfacts/QuizQuestionController.groovy
@@ -1,13 +1,43 @@
 package com.intelligrape.anatomyfacts
 
+import org.springframework.dao.DataIntegrityViolationException
+
 class QuizQuestionController {
 
-    def scaffold = QuizQuestion
+    def scaffold = true
 
-    def save() {
-        QuizQuestion question = new QuizQuestion(params)
-        if (question.save(failOnError: true))
-            flash.success = "Question <a href='${createLink(controller:'quizQuestion', action:'show', id:"${question.id}")}'>${question.id}</a> saved successfully"
-        redirect(action: 'create')
+    def save(Long id, Long version) {
+        def quizQuestion = id ? QuizQuestion.get(id) : new QuizQuestion(params)
+        if (!quizQuestion) {
+            flash.error = message(code: 'default.not.found.message', args: [message(code: 'quizQuestion.label', default: 'QuizQuestion'), id])
+            redirect(action: "list")
+            return
+        }
+
+        if (version != null) {
+            if (quizQuestion.version > version) {
+                quizQuestion.errors.rejectValue("version", "default.optimistic.locking.failure",
+                        [message(code: 'quizQuestion.label', default: 'QuizQuestion')] as Object[],
+                        "Another user has updated this QuizQuestion while you were editing")
+                render(view: "create", model: [quizQuestion: quizQuestion])
+                return
+            }
+        }
+
+        quizQuestion.properties = params
+        quizQuestion.options.eachWithIndex { option, index ->
+            option?.isCorrect = params["options[$index]"].isCorrect?params["options[$index]"].isCorrect:false
+        }
+        if (!quizQuestion.save(flush: true)) {
+            render(view: "create", model: [quizQuestion: quizQuestion])
+            return
+        }
+
+        if (id) {
+            flash.success = message(code: 'default.updated.message', args: [message(code: 'quizQuestion.label', default: 'QuizQuestion'), quizQuestion.id])
+        } else {
+            flash.success = message(code: 'default.created.message', args: [message(code: 'quizQuestion.label', default: 'QuizQuestion'), quizQuestion.id])
+        }
+        redirect(action: "show", id: quizQuestion.id)
     }
 }
--- grails-app/controllers/com/intelligrape/anatomyfacts/TestQuestionController.groovy
+++ grails-app/controllers/com/intelligrape/anatomyfacts/TestQuestionController.groovy
@@ -1,6 +1,41 @@
 package com.intelligrape.anatomyfacts
 
+import org.springframework.dao.DataIntegrityViolationException
+
 class TestQuestionController {
 
-    def scaffold = TestQuestion
+    def scaffold = true
+
+    def save(Long id, Long version) {
+        def testQuestion = id ? TestQuestion.get(id) : new TestQuestion(params)
+        if (!testQuestion) {
+            flash.error = message(code: 'default.not.found.message', args: [message(code: 'testQuestion.label', default: 'TestQuestion'), id])
+            redirect(action: "list")
+            return
+        }
+
+        if (version != null) {
+            if (testQuestion.version > version) {
+                testQuestion.errors.rejectValue("version", "default.optimistic.locking.failure",
+                        [message(code: 'testQuestion.label', default: 'TestQuestion')] as Object[],
+                        "Another user has updated this TestQuestion while you were editing")
+            render(view: "create", model: [testQuestion: testQuestion])
+            return
+            }
+        }
+
+        testQuestion.properties = params
+
+        if (!testQuestion.save(flush: true)) {
+            render(view: "create", model: [testQuestion: testQuestion])
+            return
+        }
+
+        if(id){
+            flash.success = message(code: 'default.updated.message', args: [message(code: 'testQuestion.label', default: 'TestQuestion'), testQuestion.id])
+        }else{
+            flash.success = message(code: 'default.created.message', args: [message(code: 'testQuestion.label', default: 'TestQuestion'), testQuestion.id])
+        }
+        redirect(action: "show", id: testQuestion.id)
+    }
 }
