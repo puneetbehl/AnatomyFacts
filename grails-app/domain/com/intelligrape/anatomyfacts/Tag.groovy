96fdbdd1a68ff1f39c6cb3163988e6d36529308f
Puneet <puneet.behl@intelligrape.com>
2012-10-08 08:32:51 +0530

    toString method overrided in Tag domain

--- grails-app/domain/com/intelligrape/anatomyfacts/Tag.groovy
+++ grails-app/domain/com/intelligrape/anatomyfacts/Tag.groovy
@@ -2,8 +2,12 @@ package com.intelligrape.anatomyfacts
 
 class Tag {
     String label
-
     static constraints = {
-        label(blank: false, size: 3..15, unique: true)
+        label(blank: false, unique: true, size: 30)
+    }
+
+    @Override
+    String toString() {
+        return label
     }
 }
