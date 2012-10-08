dbae7a4ffd93f188176739f9de787903698311c2
Puneet <puneet.behl@intelligrape.com>
2012-10-08 08:28:34 +0530

    quiz question controller save action modified to save options & extract tags & add them to question

--- grails-app/controllers/com/intelligrape/anatomyfacts/QuizQuestionController.groovy
+++ grails-app/controllers/com/intelligrape/anatomyfacts/QuizQuestionController.groovy
@@ -23,21 +23,27 @@ class QuizQuestionController {
                 return
             }
         }
+        collectTagsFromParams(quizQuestion)
+        quizQuestion?.properties = params
 
-        quizQuestion.properties = params
-        quizQuestion.options.eachWithIndex { option, index ->
-            option?.isCorrect = params["options[$index]"].isCorrect?params["options[$index]"].isCorrect:false
+        quizQuestion?.options?.eachWithIndex { option, index ->
+            option?.isCorrect = params["options[$index]"]?.isCorrect ? true : false
         }
         if (!quizQuestion.save(flush: true)) {
             render(view: "create", model: [quizQuestion: quizQuestion])
             return
         }
 
-        if (id) {
-            flash.success = message(code: 'default.updated.message', args: [message(code: 'quizQuestion.label', default: 'QuizQuestion'), quizQuestion.id])
-        } else {
-            flash.success = message(code: 'default.created.message', args: [message(code: 'quizQuestion.label', default: 'QuizQuestion'), quizQuestion.id])
-        }
+        flash.success = id ? message(code: 'default.updated.message', args: [message(code: 'quizQuestion.label', default: 'QuizQuestion'), quizQuestion.id]) :
+            message(code: 'default.created.message', args: [message(code: 'quizQuestion.label', default: 'QuizQuestion'), quizQuestion.id])
         redirect(action: "show", id: quizQuestion.id)
+    }
+
+    private void collectTagsFromParams(QuizQuestion quizQuestion) {
+        quizQuestion.tags = params?.tags ? params?.tags?.split(',')?.collect { label ->
+            Tag existingTag = Tag.findByLabel(label)
+            Tag tag = existingTag ? existingTag : new Tag(label: label)
+            return tag
+        } : []
     }
 }
