aed1c3313a71ef91a38d8659ba63b2c12d90234c
Puneet <puneet.behl@intelligrape.com>
2012-10-08 21:39:21 +0530

    method defined in bootstrap service & called in bootstrap init to initialize user role & UserRolegrails-app/services/com/intelligrape/anatomyfacts/BootstrapService.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,6 +3,7 @@ class BootStrap {
     def bootstrapService
 
     def init = { servletContext ->
+        bootstrapService.createSaveAndAssignRoleToUser()
         bootstrapService.createQuizQuestionsAndOptionsAndCategories()
         bootstrapService.createTips()
         bootstrapService.createTestQuestionAndOptions()
