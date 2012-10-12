89a7fa3a6eeb2019ab29f84a1c4009ca4d3d3426
Puneet <puneet.behl@intelligrape.com>
2012-10-12 11:05:41 +0530

    filters named loggedIn & notLoggedIn are created in ApplicationFilter to redirect login request to dashboard if already logged in & redirect all request except login & register to login if not logged in

--- grails-app/conf/com/intelligrape/anatomyfacts/ApplicationFilters.groovy
+++ grails-app/conf/com/intelligrape/anatomyfacts/ApplicationFilters.groovy
@@ -2,6 +2,8 @@ package com.intelligrape.anatomyfacts
 
 class ApplicationFilters {
 
+    def springSecurityService
+
     def filters = {
         all(controller: '*', action: '*') {
             before = {
@@ -12,6 +14,22 @@ class ApplicationFilters {
             before = {
                 String importStatements = """import com.intelligrape.anatomyfacts.*"""
                 session['_grails_console_last_code_'] = session['_grails_console_last_code_'] ?: importStatements
+            }
+        }
+        loggedIn(uri: "/") {
+            before = {
+                if (springSecurityService.isLoggedIn()) {
+                    redirect(controller: 'login', action: 'dashboard')
+                    return
+                }
+            }
+        }
+        notLoggedIn(uri: '/', action: 'register',invert: true) {
+            before = {
+//                if (!springSecurityService.isLoggedIn()) {
+//                    redirect(uri: "/")
+//                    return
+//                }
             }
         }
     }
