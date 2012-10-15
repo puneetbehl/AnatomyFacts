96561683bbbe775177f3d7ef1ec4a36f1647bd23
Puneet <puneet.behl@intelligrape.com>
2012-10-15 13:06:05 +0530

    removed dynamic scaffolding & added static action to QuizController

--- grails-app/controllers/com/intelligrape/anatomyfacts/QuizController.groovy
+++ grails-app/controllers/com/intelligrape/anatomyfacts/QuizController.groovy
@@ -1,18 +1,97 @@
 package com.intelligrape.anatomyfacts
 
+import org.springframework.dao.DataIntegrityViolationException
+
 class QuizController {
 
-    public static final Integer TOTAL_QUESTIONS = 50
+    public static final Integer MAX_QUESTIONS = 10
 
-    def index() { }
+    def springSecurityService
 
-    def generateQuiz() {
-        final Integer TOTAL_CATEGORIES = params?.categories?.size()
-        Collection<QuizQuestion> questions = []
-        params?.categories?.each {  categoryID ->
-            Category category = Category.load(categoryID)
-            questions.addAll(QuizQuestion.findAllByCategory(category, [max: TOTAL_QUESTIONS / TOTAL_CATEGORIES]))
+    static allowedMethods = [save: "POST", delete: "POST"]
+
+    def index() {
+        redirect(action: "list", params: params)
+    }
+
+    def generate() {
+        List<Category> categories = params?.categories?.collect {  categoryID ->
+            Category.load(categoryID)
         }
-        render(view: '/quiz/quiz', model: [questions: questions])
+        Collection<Long> questionIds = QuizQuestion.getAllQuestionIdsFromDb(categories)
+        Collections.shuffle questionIds
+        List<Long> ids = questionIds.take(questionIds.size() < MAX_QUESTIONS ? questionIds.size() : MAX_QUESTIONS)
+        List<QuizQuestion> questions = QuizQuestion.getAll(ids)
+        render(view: '/quiz/quiz', model: [questions: questions, categoryIds: params?.categories])
+    }
+
+    def result() {
+        Quiz quiz = new Quiz(params)
+        quiz.contestant = (User) springSecurityService.currentUser
+        quiz.save failOnError: true
+        Integer totalScore = 0
+        quiz?.quizAnswers?.each { qa ->
+            totalScore += qa?.answer?.isCorrect ? 1 : 0
+        }
+        render view: '/quiz/quizResult', model: [quiz: quiz, totalScore: totalScore]
+    }
+
+    def save(Long id, Long version) {
+        def quiz = id ? Quiz.get(id) : new Quiz(params)
+        if (!quiz) {
+            flash.error = message(code: 'default.not.found.message', args: [message(code: 'quiz.label', default: 'Quiz'), id])
+            redirect(action: "list")
+            return
+        }
+
+        if (version != null) {
+            if (quiz.version > version) {
+                quiz.errors.rejectValue("version", "default.optimistic.locking.failure",
+                        [message(code: 'quiz.label', default: 'Quiz')] as Object[],
+                        "Another user has updated this Quiz while you were editing")
+                render(view: "create", model: [quiz: quiz])
+                return
+            }
+        }
+
+        quiz.properties = params
+
+        if (!quiz.save(flush: true)) {
+            render(view: "create", model: [quiz: quiz])
+            return
+        }
+
+        flash.success = id ? message(code: 'default.updated.message', args: [message(code: 'quiz.label', default: 'Quiz'), quiz.id]) : message(code: 'default.created.message', args: [message(code: 'quiz.label', default: 'Quiz'), quiz.id])
+        redirect(action: "show", id: quiz.id)
+    }
+
+    def show(Long id) {
+        def quiz = Quiz.get(id)
+        if (!quiz) {
+            flash.error = message(code: 'default.not.found.message', args: [message(code: 'quiz.label', default: 'Quiz'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [quiz: quiz]
+    }
+
+    def delete(Long id) {
+        def quiz = Quiz.get(id)
+        if (!quiz) {
+            flash.error = message(code: 'default.not.found.message', args: [message(code: 'quiz.label', default: 'Quiz'), id])
+            redirect(action: "list")
+            return
+        }
+
+        try {
+            quiz.delete(flush: true)
+            flash.success = message(code: 'default.deleted.message', args: [message(code: 'quiz.label', default: 'Quiz'), id])
+            redirect(action: "list")
+        }
+        catch (DataIntegrityViolationException e) {
+            flash.error = message(code: 'default.not.deleted.message', args: [message(code: 'quiz.label', default: 'Quiz'), id])
+            redirect(action: "show", id: id)
+        }
     }
 }
