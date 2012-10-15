38502f1d8b895a5e50f45d3e2f3d8d9726b921da
Puneet <puneet.behl@intelligrape.com>
2012-10-15 13:53:07 +0530

    function to showHideButton & next, prev question button added to applcation.js

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -25,7 +25,30 @@ $(document).ready(function () {
     });
     $('.tip_content').css('width', '530px');
     $('.tip_form').css('margin', '0px 0px');
+
+    $('div.questions').hide();
+    if($('div.questions').length)   {
+        $('div#0').show().addClass('active');
+    }
+    $('li.previous').hide();
 });
+
+function nextQuestion() {
+    var divId = parseInt($('div.active').attr('id'), 10)
+    $('div#' + divId).hide().removeClass('active');
+    divId += 1;
+    $('div#' + divId).show().addClass('active');
+    showHideButton();
+}
+
+function prevQuestion() {
+    var divId = parseInt($('div.active').attr('id'), 10)
+    $('div#' + divId).hide().removeClass('active');
+    divId -= 1;
+    $('div#' + divId).show().addClass('active');
+    showHideButton();
+}
+
 function getSimilarTagsFromDB() {
     $.ajax({
         url:urls.tags_autocomplete_url,
@@ -36,4 +59,16 @@ function getSimilarTagsFromDB() {
             });
         }
     });
+}
+function showHideButton() {
+    $('ul.pager li').show()
+    if(parseInt($('div.active').attr('id'),10) == 0) {
+        $('li.next').show();
+        $('li.previous').hide();
+    }
+    if(parseInt($('div.active').attr('id'),10) == $('div.questions').length-1) {
+        $('li.next').hide();
+        $('li.previous').show();
+
+    }
 }
